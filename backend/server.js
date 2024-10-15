0ef1618be37f00c2e6b50059dd832cc514b036fa
desai-pratik <pratikdesai2274@gmail.com>
2024-10-15 05:53:03 +0530

    update live url and resolve cors error

--- backend/server.js
+++ backend/server.js
@@ -38,11 +38,10 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server started on port ${PORT}`.yellow.bold));
 
-
 const io = require("socket.io")(server, {
   pingTimeout: 120000,
   cors: {
-    origin: "https://team-collaboration-chat.onrender.com",
+    origin: "https://team-collaboration-chat.vercel.app",
     credentials: true,
   },
 });
